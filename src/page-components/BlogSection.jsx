8ae83d48fbbc8745eee5971267513f0586d781ad
AnthonyHale248 <42832023+AnthonyHale248@users.noreply.github.com>
2020-06-28 00:24:14 +0300

    items showing fix

--- src/page-components/BlogSection.jsx
+++ src/page-components/BlogSection.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 
 const BlogSection = (props) => {
    const store = useSelector(store => store);
+   let location = useLocation();
+   let articleCount = 0;
 
    const showBtn = () => {
       if (props.showBtn) {
@@ -20,14 +23,21 @@ const BlogSection = (props) => {
             <div className="blog__container">
                {
                   store.articles.map((item) => {
-                     return(
-                        <div className="blog__item" key={item.id}>
-                           <h3>{item.header}</h3>
-                           <p className="blog__item-date">{item.date}</p>
-                           <p className="blog__item-description">{item.summary}</p>
-                           <Link className="link blog__item-link" to={"/blog/" + item.header}>Read More</Link>
-                        </div>
-                     )
+                     articleCount++;
+
+                     if (articleCount < 4 || location.pathname === '/blog') {
+                        return(
+                           <div className="blog__item" key={item.id}>
+                              <h3>{item.header}</h3>
+                              <p className="blog__item-date">{item.date}</p>
+                              <p className="blog__item-description">{item.summary}</p>
+                              <Link className="link blog__item-link" to={"/blog/" + item.header}>Read More</Link>
+                           </div>
+                        )
+                     }
+                     else{
+                        return true;
+                     }
                   })
                }
             </div>
--- src/page-components/PortfolioSection.jsx
+++ src/page-components/PortfolioSection.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 
 const PortfolioSection = (props) => {
    const store = useSelector(store => store);
+   let location = useLocation();
+   let portfolioCount = 0;
 
    const showBtn = () => {
       if (props.showBtn === true) {
@@ -20,13 +23,20 @@ const PortfolioSection = (props) => {
             <div className="portfolio__container">
                {
                   store.portfolio.map((item) => {
-                     return(
-                        <Link className="portfolio__item"  to={"/portfolio/" + item.title} key={item.id}>
-                           <img src={item.imgSrc} alt="" />
-                           <h3>{item.title}</h3>
-                           <p>{item.description}</p>
-                        </Link>
-                     )
+                     portfolioCount++;
+
+                     if (portfolioCount < 5 || location.pathname === '/portfolio') {
+                        return(
+                           <Link className="portfolio__item"  to={"/portfolio/" + item.title} key={item.id}>
+                              <img src={item.imgSrc} alt="" />
+                              <h3>{item.title}</h3>
+                              <p>{item.description}</p>
+                           </Link>
+                        )
+                     }
+                     else{
+                        return true;
+                     }
                   })
                }
             </div>
